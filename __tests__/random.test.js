9e5aa2e4301ee9efc440198e6968f4e0ffff3bcf
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-02-13 14:26:18 -0600

    successful first test

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,6 +1,5 @@
-const { TestWatcher } = require('jest');
 const checkIfEqual = require('../lib/random.js');
 
-TestWatcher('checks if 10 is equal to 10', () => {
+test('checks if 10 is equal to 10', () => {
   expect(checkIfEqual(10, 10)).toBe(true);
 });
