9c7fc2754daf02ee8ac8605bff809f2a24b83aeb
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-02-14 15:11:09 -0600

    manager test suite success

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,9 +1,29 @@
 const Manager = require('../lib/Manager.js');
+const manager = new Manager('William', '36425', 'wlowrimore@gmail.com', '901');
 
-test('checks to see if manager class passes', () => {
-  const manager = new Manager("100");
-    manager.officeNumber = officeNumber;
+test('checks to see if manager class values pass', () => {
+  expect(manager.officeNumber).toBe('901');
+  expect(manager.name).toBe('William');
+  expect(manager.id).toBe('36425');
+  expect(manager.email).toBe('wlowrimore@gmail.com');
+});
 
-  expect(manager.officeNumber).toBe('100');
-  
+test('checks to see if callback: getName() method passes', () => {
+  expect(manager.getName()).toBe('William');
+});
+
+test('checks to see if callback: getId() method passes', () => {
+  expect(manager.getId()).toBe('36425');
+});
+
+test('checks to see if callback: getEmail() method passes', () => {
+  expect(manager.getEmail()).toBe('wlowrimore@gmail.com');
+});
+
+test('checks to see if callback: getOfficeNumber() method passes', () => {
+  expect(manager.getOfficeNumber()).toBe('901');
+});
+
+test('checks to see if callback: getRole() method passes', () => {
+  expect(manager.getRole()).toBe('Manager');
 });
